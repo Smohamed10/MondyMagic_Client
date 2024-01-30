3cfc3cb89a53dbdbf3b9d46035707bac88f5160f
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-01-30 03:50:32 +0200

    return

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -14,88 +14,80 @@ const Header = () => {
     }
 
     return (
-  <div class="site-wrap">
-      <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
+<div>
+<header className="site-navbar md-5 " role="banner">
+<div className="container">
+  <div className="row align-items-center">
     
-      <header class="site-navbar py-1" role="banner">
+    <div className="col-6 col-xl-2">
 
-      <div class="container">
-        <div class="row align-items-center">
-          
-          <div class="col-6 col-xl-2">
-            <h1 class="mb-0"><a href="/" class="text-black h2 mb-0">Mondy<span className='Ankh'>&#9765;</span>Magic</a></h1>
-          </div>
-          <div class="col-10 col-md-8 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right text-lg-center" role="navigation">
-
-              <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
-                <li class="active">
-                  <a href="/">Home</a>
-                </li>                
-                {
-                  !Auth &&
-                    <li><a href="/Login">Login</a></li>
-                }
-      
-                {
-                  !Auth &&
-                    <li><a href="/Register">Register</a></li>
-                }
-      
-                <li><a href="/contact">Contact</a></li>
-      
-                
-                {
-                 Auth && Auth[0].status===1 && 
-                <li><a href="/managetrips">Manage trips</a></li>
-                
-                }
-                {
-                 Auth && Auth[0].status===1 && 
-                <li><a href="/managebookings">Manage Bookings</a></li>
-                
-                }
-              </ul>
-            </nav>
-          </div>
-
-          <div class="col-6 col-xl-2 text-right">
-            <div class="d-none d-xl-inline-block">
-              <ul className="site-menu js-clone-nav ml-auto list-unstyled d-flex text-right mb-0" data-classname="social">
-                <li>
-                  <a href="https://www.facebook.com/mondy.rmadan?mibextid=LQQJ4d" className="pl-3 pr-3 text-black"><span className="icon-facebook"></span></a>
-                </li>
-                <li>
-                  <a href="https://wa.me/+2001009445487" className="pl-3 pr-3 text-black"><span className="icon-whatsapp"></span></a>
-                </li>
-                <li>
-                  <a href="https://www.instagram.com/mondyrmadan?igsh=MTdtOWwwcTJzcjY0eA==" className="pl-3 pr-3 text-black"><span className="icon-instagram"></span></a>
-                </li>
-                {
-                  Auth &&
-                    <button onClick={Logout} className='btn btn-sm btn-danger mx-2'>Logout</button>
-                  
-                }
-              
-              </ul>
-            </div>
-
-            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-          </div>
-
-        </div>
-      </div>
-      
-    </header>
+      <h1 className="mb-0"><a href='/' className="text-black h2 mb-0" style={{ cursor: 'pointer' }}>
+                Mondy<span className='Ankh'>&#9765;</span>Magic
+            </a></h1>
     </div>
+    <div className="col-10 col-md-8 d-none d-xl-block">
+      <nav className="site-navigation position-relative text-right text-lg-center" role="navigation">
+
+        <ul className="site-menu js-clone-nav mx-auto d-none d-lg-block">
+          <li className=""><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          {
+            !Auth &&
+              <li><a href="/Login">Login</a></li>
+          }
+
+          {
+            !Auth &&
+              <li><a href="/Register">Register</a></li>
+          }
+
+          <li><a href="/contact">Contact</a></li>
+
+          
+          {
+           Auth && Auth[0].status===1 && 
+          <li><a href="/managetrips">Manage trips</a></li>
+          
+          }
+          {
+           Auth && Auth[0].status===1 && 
+          <li><a href="/managebookings">Manage Bookings</a></li>
+          
+          }
+        </ul>
+      </nav>
+    </div>
+
+    <div className="col-6 col-xl-2 text-right">
+      <div className="d-none d-xl-inline-block">
+        <ul className="site-menu js-clone-nav ml-auto list-unstyled d-flex text-right mb-0" data-classname="social">
+          <li>
+            <a href="https://www.facebook.com/mondy.rmadan?mibextid=LQQJ4d" className="pl-3 pr-3 text-black"><span className="icon-facebook"></span></a>
+          </li>
+          <li>
+            <a href="https://wa.me/+2001009445487" className="pl-3 pr-3 text-black"><span className="icon-whatsapp"></span></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/mondyrmadan?igsh=MTdtOWwwcTJzcjY0eA==" className="pl-3 pr-3 text-black"><span className="icon-instagram"></span></a>
+          </li>
+          {
+            Auth &&
+              <button onClick={Logout} className='btn btn-sm btn-danger mx-2'>Logout</button>
+            
+          }
+        
+        </ul>
+      </div> 
+
+      <div className="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style={{position: "relative", top: "3px"}}><a href=" " className="site-menu-toggle js-menu-toggle text-black"><span className="icon-menu h3"></span></a></div>
+
+    </div>
+
+  </div>
+</div>
+
+</header>
+</div>
     );
 };
 
