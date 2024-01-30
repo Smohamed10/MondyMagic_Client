73b4ee8f2dde0011d50a604b1bf31f1eb4666f54
Sherif Elkereti <101013434+Smohamed10@users.noreply.github.com>
2024-01-30 04:24:39 +0200

    ;;;;

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import React from 'react';
 function App() {
   return (
     <div className="site-wrap">
+              <div className="site-mobile-menu">
+          <div className="site-mobile-menu-header">
+            <div className="site-mobile-menu-close mt-3">
+              <span className="icon-close2 js-menu-toggle"></span>
+            </div>
+          </div>
+          <div className="site-mobile-menu-body"></div>
+        </div>
     <Header/>
     <Outlet/>
     <Footer/>
